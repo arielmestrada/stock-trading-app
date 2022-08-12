3e045b095d8b4dde9d36313b692e645f700bf158
JohnDv <johndominicvillalino@outlook.com>
2022-08-12 19:11:02 +0800

    dashboard init design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+    def get_all_trans(role)
+        if role == 'trader'
+            @transactions = Transaction.where(user_id:current_user.id)
+        else
+            @transactions = Transaction.all     
+        end
+    end
+
+
 end
