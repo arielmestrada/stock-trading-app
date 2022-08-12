2bd14647330a939bf0418bf00d122e55d9f69ef2
JohnDv <johndominicvillalino@outlook.com>
2022-08-12 19:29:12 +0800

    fix db bug

--- db/migrate/20220812043302_drop_tables.rb
+++ db/migrate/20220812043302_drop_tables.rb
@@ -2,7 +2,6 @@ class DropTables < ActiveRecord::Migration[7.0]
   def change
     drop_table :users, force: :cascade
     drop_table :listings, force: :cascade
-    drop_table :stocks, force: :cascade
     drop_table :transactions, force: :cascade
   end
 end
